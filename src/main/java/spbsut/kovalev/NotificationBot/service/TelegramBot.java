92c241e9a9a33dd5e6804b460c63be30582018f8
Nikolay Kovalev <Nikolay.Kovalev.SPB@mail.ru>
2024-05-06 14:24:56 +0300

    Добавляю обработку команды start

--- src/main/java/spbsut/kovalev/NotificationBot/service/TelegramBot.java
+++ src/main/java/spbsut/kovalev/NotificationBot/service/TelegramBot.java
@@ -2,7 +2,9 @@ package spbsut.kovalev.NotificationBot.service;
 
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
@@ -16,8 +18,38 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText())  {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
 
+            switch (messageText) {
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                default:
+                    sendMessage(chatId, "Извините, команда не была распознана");
+                    break;
+            }
+        }
     }
+
+    private void startCommandReceived(long chatId, String firstName) {
+        String answer = STR."Привет, \{firstName}, приятно познакомиться!";
+        sendMessage(chatId, answer);
+    }
+
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(textToSend);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
     @Override
     public String getBotUsername() {
