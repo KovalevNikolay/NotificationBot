f5151eaefc6102e0d1712dd75017cf03d8b71e0a
Nikolay Kovalev <Nikolay.Kovalev.SPB@mail.ru>
2024-05-06 13:13:39 +0300

    Initial commit

---
+++ src/main/java/spbsut/kovalev/NotificationBot/NotificationBotApplication.java
@@ -0,0 +1,13 @@
+package spbsut.kovalev.NotificationBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationBotApplication.class, args);
+	}
+
+}
